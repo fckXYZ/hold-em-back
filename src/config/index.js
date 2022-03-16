cebaa3d344cc3befef6c2b45f2295a6f70bba30d
Artem Kovtun <rtmkvtn@gmail.com>
2022-03-16 16:43:39 +0300

    rm creds from config

--- src/config/index.js
+++ src/config/index.js
@@ -3,6 +3,8 @@ const {
   NODE_ENV,
   JWT_SECRET,
   MONGO_DB_ADDRESS,
+  DB_USERNAME,
+  DB_PASSWORD,
 } = process.env;
 const rateLimit = require('express-rate-limit');
 
@@ -16,6 +18,6 @@ module.exports = {
   DEFAULT_LANGUAGE: 'en',
   PORT,
   JWT_SECRET: NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret-phrase',
-  MONGO_DB_ADDRESS: NODE_ENV === 'production' ? MONGO_DB_ADDRESS : 'mongodb+srv://fckxyz:tkolo4hd@test.dyfrr.mongodb.net/hold-em-db?retryWrites=true&w=majority',
+  MONGO_DB_ADDRESS: NODE_ENV === 'production' ? MONGO_DB_ADDRESS : `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@test.dyfrr.mongodb.net/hold-em-db?retryWrites=true&w=majority`,
   limiter,
 };
